cfae20791d487f13591f4d0089a942046fad633d
leonidgerasim <porovozov.987654321@mail.ru>
2025-07-17 01:29:54 +0300

    Реализован класс Egine

---
+++ engine.py
@@ -0,0 +1,12 @@
+class Environment:
+    def __init__(self, road_slop=0):
+        self.accr_ped = 0
+        self.brk_ped = 0
+        self.road_slop = road_slop
+
+    def acceleration(self):
+        
+
+    def breaking(self):
+        pass
+
